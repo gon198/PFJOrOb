c3e82a6352c82e2ab5435f6f2000d8a55b85a9fc
gon198 <gonzalomonter@gmail.com>
2014-11-27 16:56:30 -0600

    Create Artista.java

---
+++ Artista.java
@@ -0,0 +1,14 @@
+public class Artista{
+	private String nombre;
+	private String genero;
+	private int edad;
+	private String hist;
+	
+	public Artista(String nombre, String genero, int edad, String hist){
+		this.nombre=nombre;
+		this.genero=genero;
+		this.edad=edad;
+		this.hist=hist;
+	}
+	
+}
